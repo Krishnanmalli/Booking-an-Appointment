0a0db7659d31a843d22e540bea38881b777d7cb5
Krishnanmalli <mallikrishnan125@gmail.com>
2025-06-13 22:04:38 +0530

    Update BookingAppointmentwithTest.java

--- BookingAppointmentwithTest.java
+++ BookingAppointmentwithTest.java
@@ -26,29 +26,27 @@ public class BookingAppointmentwithTest {
 		driver.findElement(By.id("btn-make-appointment")).click();
 		driver.findElement(By.id("txt-username")).sendKeys("John Doe");
 		driver.findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
-        driver.findElement(By.id("btn-login")).click();
+                driver.findElement(By.id("btn-login")).click();
 		Thread.sleep(500);
-        driver.findElement(By.id("combo_facility")).click();
-        Select Facility = new Select(driver.findElement(By.id("combo_facility")));
+                driver.findElement(By.id("combo_facility")).click();
+                Select Facility = new Select(driver.findElement(By.id("combo_facility")));
 		Facility.selectByValue("Hongkong CURA Healthcare Center");
 		Thread.sleep(500);
-	    driver.findElement(By.id("radio_program_medicaid")).click();
+	        driver.findElement(By.id("radio_program_medicaid")).click();
 		Thread.sleep(500);
-	    driver.findElement(By.id("txt_visit_date")).click();
+	        driver.findElement(By.id("txt_visit_date")).click();
 		Thread.sleep(7000);
-	    driver.findElement(By.xpath("/html/body/div/div[1]/table/thead/tr[2]/th[2]")).click();
-        driver.findElement(By.xpath("/html/body/div/div[2]/table/tbody/tr/td/span[6]")).click();
-        driver.findElement(By.xpath("/html/body/div/div[1]/table/tbody/tr[4]/td[4]")).click();
-        driver.findElement(By.id("txt_comment")).sendKeys("Looking forward to the appointment. Please confirm the timing");
-        driver.findElement(By.id("btn-book-appointment")).click();
+	        driver.findElement(By.xpath("/html/body/div/div[1]/table/thead/tr[2]/th[2]")).click();
+                driver.findElement(By.xpath("/html/body/div/div[2]/table/tbody/tr/td/span[6]")).click();
+                driver.findElement(By.xpath("/html/body/div/div[1]/table/tbody/tr[4]/td[4]")).click();
+                driver.findElement(By.id("txt_comment")).sendKeys("Looking forward to the appointment. Please confirm the timing");
+                driver.findElement(By.id("btn-book-appointment")).click();
 	}
 
 	@AfterMethod
 	public void logout() throws InterruptedException{
 		driver.findElement(By.xpath("//*[@id=\"menu-toggle\"]/i")).click();
-        driver.findElement(By.xpath("//*[@id=\"sidebar-wrapper\"]/ul/li[5]/a")).click();
-        driver.quit();
+                driver.findElement(By.xpath("//*[@id=\"sidebar-wrapper\"]/ul/li[5]/a")).click();
+                driver.quit();
 	}
-
 }
-
